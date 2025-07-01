7eacaa277a487312240fdde00c6dcf6f357c9027
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2025-07-01 16:22:32 +0100

    First full version of workflow execution

--- spinnirker_models/src/main.c
+++ spinnirker_models/src/main.c
@@ -23,6 +23,7 @@
 #include "workflow.h"
 
 #include <spin1_api.h>
+#include <bit_field.h>
 #include <data_specification.h>
 #include <simulation.h>
 #include <recording.h>
@@ -31,6 +32,8 @@
 enum REGIONS {
     //! The system region
     SYSTEM_REGION = 0,
+    //! The configuration region
+    CONFIG_REGION,
     //! The workflow region
     WORKFLOW_REGION,
     //! The recording metadata region
@@ -38,11 +41,28 @@ enum REGIONS {
 };
 
 enum PRIORITY {
+    // USER and SDP are background tasks
+    USER = 1,
     SDP = 1,
-    TIMER = 1,
+    // Timer and DMA should interrupt the background tasks
+    TIMER = 0,
     DMA = 0,
+    // The Multicast callback should interrupt everything to get the data off
+    // the network.
     MULTICAST = -1,
+    // The simulation should not set up DMA as this is handled separately
+    SIM_DMA = -2
 };
+
+typedef struct {
+    //! The maximum number of input packets
+    uint32_t max_input_packets;
+} config_t;
+
+typedef struct {
+    uint32_t n_packets;
+    uint32_t *packets;
+} packet_buffer_t;
 
 //! The timer period in microseconds
 static uint32_t timer_period;
@@ -62,6 +82,22 @@ static uint32_t time;
 //! A configured workflow to be run
 static workflow_t *workflow;
 
+//! The main configuration
+static config_t config;
+
+//! Buffers of packets received but yet to be processed;
+//! we keep 2 of them to swap out each timer tick
+static packet_buffer_t input_packets[2];
+
+//! The index of the buffer to put spikes in now
+static uint32_t write_buffer = 0;
+
+//! The index of the buffer to read spike from now
+static uint32_t read_buffer = 0;
+
+//! The number of packet buffer overflows
+static uint32_t n_packet_overflows = 0;
+
 /**
  * \brief Set up the workflow.
  */
@@ -79,7 +115,7 @@ static bool initialize(void) {
     if (!simulation_initialise(
             data_specification_get_region(SYSTEM_REGION, ds_regions),
             APPLICATION_NAME_HASH, &timer_period, &simulation_ticks,
-            &infinite_run, &time, SDP, DMA)) {
+            &infinite_run, &time, SDP, SIM_DMA)) {
         return false;
     }
 
@@ -95,6 +131,23 @@ static bool initialize(void) {
             ds_regions), &workflow)) {
         return false;
     }
+
+    // Set up the input spike buffer
+    config_t *sdram_config = data_specification_get_region(CONFIG_REGION,
+            ds_regions);
+    config = *sdram_config;
+    for (uint32_t i = 0; i < 2; i++) {
+        input_packets[i].n_packets = 0;
+        input_packets[i].packets = spin1_malloc(sizeof(uint32_t) *
+                config.max_input_packets);
+        if (!input_packets[i].packets) {
+            log_error("Failed to allocate packet buffer of %u packets",
+                    config.max_input_packets);
+            return false;
+        }
+    }
+    read_buffer = 0;
+    write_buffer = 0;
     return true;
 }
 
@@ -102,6 +155,33 @@ static void resume_callback(void) {
     if (recording_flags) {
         recording_reset();
     }
+}
+
+static void start_workflow(void) {
+    // If the workflow is currently running, just mark it to restart
+    if (workflow->running) {
+        workflow->restart = true;
+        return;
+    }
+
+    // If the workflow has SDRAM inputs, start these running
+    if (workflow->n_sdram_inputs > 0) {
+        workflow->running = true;
+        workflow->n_input_dmas_done = 0;
+        workflow->n_output_dmas_done = 0;
+        setup_read_dma(workflow);
+
+        // If the first component doesn't need SDRAM inputs, start running
+        if (workflow->components[0].n_input_dmas_needed == 0) {
+            spin1_trigger_user_event(0, 0);
+        }
+
+        return;
+    }
+
+    // The workflow must be ready to run, so start it
+    workflow->running = true;
+    spin1_trigger_user_event(0, 0);
 }
 
 void timer_callback(uint time, uint unused0) {
@@ -122,25 +202,57 @@ void timer_callback(uint time, uint unused0) {
         return;
     }
 
-    // Run the workflow for this timer tick
-    run_workflow(workflow);
+    // Switch over the packet buffers
+    read_buffer = write_buffer;
+    write_buffer = (write_buffer + 1) & 0x1;
 
-    if (workflow->output_recorded) {
-        // If record output, do recording
-        if (workflow->output_spikes) {
-            // TODO: Record spikes
-        } else {
-            // TODO: Record data
-        }
-    } else if (workflow->output_spikes) {
-        // Otherwise if the output is spikes, we need to send spikes
-        // TODO: Send spikes
+    // If we need to wait for a packet to start the workflow, do nothing
+    if (workflow->wait_for_start_key) {
+        return;
     }
+    start_workflow();
 }
 
 void multicast_packet_callback(uint key, uint payload) {
     use(key);
     use(payload);
+    // If the workflow is waiting for a start key, check if this is it
+    if (workflow->wait_for_start_key && key == workflow->start_key) {
+        start_workflow();
+        return;
+    }
+
+    // Store the spikes, or not if already full
+    packet_buffer_t *packet_buffer = &input_packets[write_buffer];
+    if (packet_buffer->n_packets >= config.max_input_packets) {
+        n_packet_overflows++;
+        return;
+    }
+    packet_buffer->packets[packet_buffer->n_packets++] = key;
+}
+
+void user_callback(uint unused0, uint unused1) {
+    use(unused0);
+    use(unused1);
+
+    // If we are about to start component 0, first process any packets
+    if (workflow->next_component == 0) {
+        // Process the spikes in the current buffer
+        packet_buffer_t *packet_buffer = &input_packets[read_buffer];
+        for (uint32_t i = 0; i < packet_buffer->n_packets; i++) {
+            process_packet(time, packet_buffer->packets[i], workflow);
+        }
+        packet_buffer->n_packets = 0;
+    }
+
+    run_workflow(workflow);
+}
+
+void dma_callback(uint unused0, uint tag) {
+    use(unused0);
+    dma_id_t id;
+    id.id = tag;
+    process_dma_complete(id, workflow);
 }
 
 //! Entry point
@@ -160,8 +272,9 @@ void c_main(void) {
 
     // Register callback
     spin1_callback_on(TIMER_TICK, timer_callback, TIMER);
-    spin1_callback_on(
-            MC_PACKET_RECEIVED, multicast_packet_callback, MULTICAST);
+    spin1_callback_on(MC_PACKET_RECEIVED, multicast_packet_callback, MULTICAST);
+    spin1_callback_on(USER_EVENT, user_callback, USER);
+    spin1_callback_on(DMA_TRANSFER_DONE, dma_callback, DMA);
 
     simulation_run();
 }
--- spinnirker_models/src/workflow.h
+++ spinnirker_models/src/workflow.h
@@ -1,10 +1,10 @@
+// DO NOT EDIT! THIS FILE WAS GENERATED FROM ../../src/workflow.h
+
 /*
  Copyright (c) 2025 The University of Manchester
-
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
      https://www.apache.org/licenses/LICENSE-2.0
 
  Unless required by applicable law or agreed to in writing, software
@@ -18,6 +18,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <bit_field.h>
 
 //! \brief Defines a function type for components in a workflow.
 //! Note no input is the same as the output.
@@ -48,6 +49,76 @@ typedef struct {
     component_deinit deinit;
 } component_t;
 
+//! Information about the key and mask for a spike input
+typedef struct {
+    //! The key for the spike input
+    uint32_t key;
+    //! The mask for the spike input
+    uint32_t mask;
+    //! The number of bits of key used for colour (0 if no colour)
+    uint32_t n_colour_bits: 3;
+    //! The shift to apply to the key to get the core part
+    uint32_t mask_shift: 13;
+    //! The mask to apply to the key once shifted to get the core index
+    uint32_t core_mask: 16;
+    //! The number of atoms per core
+    uint32_t n_per_core;
+} key_info_t;
+
+typedef struct {
+    // The source ID of the spike
+    uint32_t global_source_id: 28;
+    // The delay of the spike in reaching this core from the source core
+    uint32_t delay: 4;
+} spike_t;
+
+typedef struct {
+    //! The number of spikes in the list
+    uint32_t n_spikes;
+    //! The spikes in the list
+    spike_t spikes[];
+} spike_list_t;
+
+//! An input that receives spikes.
+typedef struct {
+    //! Information about the key and mask for the spike input
+    key_info_t key_info;
+    //! The maximum number of spikes to allow in the input
+    uint32_t max_spikes;
+    //! The location where the spikes are stored
+    spike_list_t *spikes;
+    //! The number of spikes lost to overflow
+    uint32_t n_spikes_overflow_lost;
+    //! The number of spikes lost to delay overflow
+    uint32_t n_spikes_delay_lost;
+    //! The number of spikes received
+    uint32_t n_spikes_received;
+} spike_input_t;
+
+//! An input that receives data from SDRAM using DMA
+typedef struct {
+    //! The address in SDRAM to read from
+    void *address;
+    //! The size of the data to read, in bytes
+    uint32_t size_in_bytes;
+    //! The local memory allocated for the input data
+    void *local_data;
+    //! The number of components that the SDRAM input targets
+    uint32_t n_targets;
+    //! List of component IDs targeted
+    uint32_t *target_components;
+} sdram_input_t;
+
+//! An output that sends data to SDRAM using DMA
+typedef struct {
+    //! The address in SDRAM to write to
+    void *address;
+    //! The size of the data to write, in bytes
+    uint32_t size_in_bytes;
+    //! The sources of the SDRAM output
+    uint32_t component_index;
+} sdram_output_t;
+
 //! A component in a workflow
 typedef struct {
     //! Function to call for this component
@@ -60,27 +131,86 @@ typedef struct {
     void **input;
     //! For each input how much data to copy (0 for no copy)
     uint32_t *copy_data_size;
-    //! Pointers to data to copy from the output to the input
+    //! Pointers to data to copy to the input
     void **data_to_copy;
+    //! The number of DMAs needed before starting this component
+    uint32_t n_input_dmas_needed;
+    //! The number of DMAs done before starting this component
+    uint32_t n_input_dmas_done;
     //! Output data pointer
     void *output;
 } workflow_component_t;
 
 typedef struct {
-    // Whether the input to the workflow is spikes
-    bool input_spikes;
-    //! Whether the output of the workflow is spikes
-    bool output_spikes;
-    //! Whether the output of the workflow is recorded or sent (only spikes)
-    bool output_recorded;
-    //! The base key to send spikes with if output_spikes is true and
-    //! output_recorded is false
-    uint32_t base_key;
+    //! The number of spike inputs into the workflow
+    uint32_t n_spike_inputs;
+    //! The spike inputs into the workflow
+    spike_input_t *spike_inputs;
+    //! The number of SDRAM inputs into the workflow
+    uint32_t n_sdram_inputs;
+    //! The SDRAM inputs into the workflow
+    sdram_input_t *sdram_inputs;
+    //! The number of SDRAM outputs from the workflow
+    uint32_t n_sdram_outputs;
+    //! The SDRAM outputs from the workflow
+    sdram_output_t *sdram_outputs;
     //! The number of components in the workflow
     uint32_t n_components;
     //! The components in the workflow
     workflow_component_t *components;
+    //! The number of input DMA operations done
+    uint32_t n_input_dmas_done;
+    //! The number of output DMA operations done
+    uint32_t n_output_dmas_done;
+    //! The next component to be run
+    uint32_t next_component;
+    //! Whether the workflow is currently running
+    bool running;
+    //! Whether the workflow should be restarted on completion
+    bool restart;
+    //! Whether to send the next key on completion
+    bool send_next_key;
+    //! Whether to wait for the start key to start
+    bool wait_for_start_key;
+    //! The next key to send on completion
+    uint32_t next_key;
+    //! The key that indicates the start of simulation
+    uint32_t start_key;
 } workflow_t;
+
+//! An input that receives spikes.
+typedef struct {
+    //! Information about the key and mask for the spike input
+    key_info_t key_info;
+    //! The maximum number of spikes to allow in the input
+    uint32_t max_spikes;
+    //! The number of components that the spike input targets
+    uint32_t n_targets;
+    //! The index of the components that the spike input targets
+    uint32_t target_components[];
+} spike_input_config_t;
+
+//! An input that receives data from SDRAM using DMA
+typedef struct {
+    //! The address in SDRAM to read from
+    uint32_t address;
+    //! The size of the data to read, in bytes
+    uint32_t size_in_bytes;
+    //! The number of components that the SDRAM input targets
+    uint32_t n_targets;
+    //! The indices of the component that the SDRAM input targets
+    uint32_t target_components[];
+} sdram_input_config_t;
+
+//! An output that sends data to SDRAM using DMA
+typedef struct {
+    //! The address in SDRAM to write to
+    uint32_t address;
+    //! The size of the data to write, in bytes
+    uint32_t size_in_bytes;
+    //! The source component of the output data to copy
+    uint32_t component_index;
+} sdram_output_config_t;
 
 //! The configuration of a component in a workflow.
 typedef struct {
@@ -97,27 +227,52 @@ typedef struct {
     // The number of outputs from this component
     uint32_t n_outputs;
     // The indices of the next components that this component feeds into
-    uint32_t next_components[];
-    // The parameter data for this component follows
+    uint32_t outputs[];
+    // The parameter data for this component follows; this has undefined length
     // void params[];
 } workflow_component_config_t;
 
 //! The configuration of a workflow, which consists of multiple components.
 typedef struct {
-    // Whether the input to the workflow is spikes
-    bool input_spikes;
-    //! Whether the output of the workflow is spikes
-    bool output_spikes;
-    //! Whether the output of the workflow is recorded or sent (only spikes)
-    bool output_recorded;
-    //! The base key to send spikes with if output_spikes is true and
-    //! output_recorded is false
-    uint32_t base_key;
+    // The number of spike inputs to this component
+    uint32_t n_spike_inputs;
+    // The number of SDRAM inputs to this component
+    uint32_t n_sdram_inputs;
+    // The number of SDRAM outputs from this component
+    uint32_t n_sdram_outputs;
     //! The number of components in the workflow
     uint32_t n_components;
-    //! The components in the workflow
-    workflow_component_config_t components[];
+    //! Whether to send the next key on completion
+    bool send_next_key;
+    //! Whether to wait for the start key to start
+    bool wait_for_start_key;
+    //! The next key to send on completion
+    uint32_t next_key;
+    //! The key that indicates the start of simulation
+    uint32_t start_key;
+    // A pointer to data to make finding the rest of the data easier
+    uint32_t data[];
+    // The spike inputs for this component follows
+    // spike_input_config_t spike_inputs[n_spike_inputs];
+    // The SDRAM inputs for this component follows
+    // sdram_input_config_t sdram_inputs[n_sdram_inputs];
+    // The SDRAM outputs for this component follows
+    // sdram_output_config_t sdram_outputs[n_sdram_outputs];
+    // The components in the workflow follows; this has undefined length
+    // workflow_component_config_t components[];
 } workflow_config_t;
+
+//! \brief A union that can be used to identify a DMA operation
+typedef union {
+    //! The ID of the DMA operation
+    uint32_t id;
+    struct {
+        //! The index of the element this is a transfer for
+        uint32_t index: 31;
+        //! Whether this is an input (1) or output (0)
+        uint32_t is_input: 1;
+    };
+} dma_id_t;
 
 //! \brief Configures a set of workflow components based on the provided
 //!        configuration.
@@ -125,6 +280,22 @@ typedef struct {
 //! \param[out] workflow: Pointer to a configured workflow pointer
 //! \return True if the configuration was successful, false otherwise
 bool configure_workflow(workflow_config_t *config, workflow_t **workflow);
+
+//! \brief Processes a packet in the workflow.
+//! \param[in] time: The current simulation time
+//! \param[in] key: The key of the packet to process
+//! \param[in] workflow: Pointer to a configured workflow
+//! \return True if the packet was processed successfully, false otherwise
+bool process_packet(uint32_t time, uint32_t key, workflow_t *workflow);
+
+//! \brief Sets up the next read operation for the workflow.
+//! \param[in] workflow: Pointer to a configured workflow
+void setup_read_dma(workflow_t *workflow);
+
+//! \brief Processes the result of a DMA completion.
+//! \param[in] id: The ID of the DMA operation that completed
+//! \param[in] workflow: Pointer to a configured workflow
+void process_dma_complete(dma_id_t id, workflow_t *workflow);
 
 //! \brief Runs a set of workflow components.  Note that the order is expected
 //!        to have been determined before this call, and that all inputs and
